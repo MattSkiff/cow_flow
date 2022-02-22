6a4b2690740ef208efceb4f1019bf3d0310d408e
Matthew Skiffington <42245625+MattSkiff@users.noreply.github.com>
2022-02-23 12:30:20 +1300

    scrap battery code

--- mnist.py
+++ mnist.py
@@ -6,7 +6,7 @@ from torch.cuda import empty_cache
 from torch.utils.data import DataLoader # Dataset                                                                                                                                                                    
 # from torchvision import transforms
 import config as c
-from train import train, train_battery
+from train import train
 
 from torch.utils.data.sampler import SubsetRandomSampler # RandomSampling
 
@@ -29,28 +29,10 @@ def train_mnist(load_only=False):
     else:
         toy_sampler = None
     
-    if len(c.batch_size) == 1:
-        train_loader = DataLoader(mnist_train,batch_size = c.batch_size[0],pin_memory=True,
+    train_loader = DataLoader(mnist_train,batch_size = c.batch_size[0],pin_memory=True,
                                       shuffle=False,sampler=toy_sampler)
-        val_loader = DataLoader(mnist_test,batch_size = c.batch_size[0],pin_memory=True,
+    val_loader = DataLoader(mnist_test,batch_size = c.batch_size[0],pin_memory=True,
                                       shuffle=False,sampler=toy_sampler)
-        if len(c.lr_init) == 1:
-            mdl = train(train_loader,val_loader,lr_i=c.lr_init)
-        else:
-            mdl = train_battery([train_loader],[val_loader],lr_i=c.lr_init)
-                
-    else:
-        tls,vls = [],[]
-        
-        for bs in c.batch_size:
-            tls.append(DataLoader(mnist_train,batch_size = bs,pin_memory=True,
-                                      shuffle=False,sampler=toy_sampler))
-            vls.append(DataLoader(mnist_test,batch_size = bs,pin_memory=True,
-                                      shuffle=False,sampler=toy_sampler))
-            
-            if not load_only:
-                mdl = train_battery(tls,vls,lr_i=c.lr_init)
-            else:
-                mdl = None
+    mdl = train(train_loader,val_loader,lr_i=c.lr_init)
     
     return mdl, train_loader, val_loader
