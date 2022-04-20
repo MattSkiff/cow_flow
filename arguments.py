9d72a2a9ba5be3be7fc48f09bf93fa8878fcb074
Matthew Skiffington <mks29@students.waikato.ac.nz>
2022-04-20 11:50:46 +1000

    fix bug

--- arguments.py
+++ arguments.py
@@ -4,7 +4,7 @@ import socket
 # command line params
 parser = argparse.ArgumentParser(description='Create dataloaders and train a model on MNIST, DLRACD or cow dataset.')
 
-parser.add_argument('-mod',"--model_name",help="Specify model to train (NF, CSRNet, UNet, FCRN, LCFCN).",default='UNet')
+parser.add_argument('-mod',"--model_name",help="Specify model to train (NF, CSRNet, UNet, FCRN, LCFCN).",default='LCFCN')
 parser.add_argument("-fe_only", "--feat_extract_only", help="Trains the feature extractor component only.", action="store_true",default=False)
 parser.add_argument("-uc", "--unconditional", help="Trains the model without labels.", action="store_true")
 parser.add_argument("-gn", "--gpu_number", help="Selects which GPU to train on.", type=int, default=0)
