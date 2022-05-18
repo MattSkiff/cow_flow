79b93b9162df80d15a7605e3bd3bb8132ebe0940
Matthew Skiffington <mks29@students.waikato.ac.nz>
2022-05-18 12:23:39 +1000

    fix eval localisation code, add split dir func

---
+++ predict.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 16 17:08:01 2022
+
+@author: mks29
+"""
+import utils
+
+utils.predict_image('/home/mks29/clones/cow_flow/models/best_9S1_UNet_seg_ml-17_BS16_LR_I0.0002_E500_DIM256_OPTIMsgd_weight_MX_SZ_4_none_11_05_2022_13_13_32',mdl_type='UNet_seg')
