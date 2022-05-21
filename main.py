2356c25085ae48cdef37478ccd0819e99aadce10
Matthew Skiffington <mks29@students.waikato.ac.nz>
2022-05-21 22:56:30 +1000

    add holdout option

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # external
-import torch
 from torch.cuda import empty_cache
 from torch.utils.data import DataLoader # Dataset                                                                                                                                                                    
-from torch.utils.data.sampler import SubsetRandomSampler # RandomSampling
+
 import torch.nn as nn
 import os
 
@@ -10,7 +9,7 @@ import os
 import config as c
 import arguments as a
 import model
-from data_loader import CustToTensor, AerialNormalize, DmapAddUniformNoise, train_val_split, Resize, RotateFlip, CustResize, prep_transformed_dataset
+from data_loader import prep_transformed_dataset, make_loaders
 from train import train, train_baselines, train_feat_extractor, train_classification_head
 from utils import load_model
 from eval import dmap_metrics, eval_baselines
@@ -28,65 +27,20 @@ if a.args.data == 'mnist':
 if a.args.data == 'cows':
     # torchivsion inputs are 3x227x227, mnist_resnet 1x227...
     # 0.1307, 0.3081 = mean, std dev mnist
-    
-    transforms = [CustToTensor()]
-    
-    if a.args.normalise:
-        transforms.append(AerialNormalize())
-    
-    if a.args.resize:
-        transforms.append(Resize())
-    
-    if not a.args.resize:
-        transforms.append(CustResize())
-    
-    #if a.args.rrc:
-    if not a.args.mode == 'eval':
-        transforms.append(RotateFlip())
-        
-    transforms.append(DmapAddUniformNoise())
 
-    transformed_dataset = prep_transformed_dataset(transforms)
+    transformed_dataset = prep_transformed_dataset()
     
     # check dataloader if running interactively
     if any('SPYDER' in name for name in os.environ):
-        transformed_dataset.show_annotations(5895) #
-        #transformed_dataset.show_annotations(0) #
+        transformed_dataset.show_annotations(66) # 5895
     
-    # create test train split
-    t_indices, t_weights, v_indices, v_weights  = train_val_split(dataset = transformed_dataset,
-                                                      train_percent = c.test_train_split,
-                                                      annotations_only = (a.args.sampler == 'anno'),
-                                                      seed = c.seed,
-                                                      oversample= (a.args.sampler == 'weighted'))
-    
-    f_t_indices, f_t_weights, f_v_indices, f_v_weights  = train_val_split(dataset = transformed_dataset,
-                                                      train_percent = c.test_train_split,
-                                                      annotations_only = False,
-                                                      seed = c.seed,
-                                                      oversample=False)
-    train_sampler = SubsetRandomSampler(t_indices,generator=torch.Generator().manual_seed(c.seed))
-    val_sampler = SubsetRandomSampler(v_indices,generator=torch.Generator().manual_seed(c.seed))
-  
-    full_train_sampler = SubsetRandomSampler(f_t_indices,generator=torch.Generator().manual_seed(c.seed))
-    full_val_sampler = SubsetRandomSampler(f_v_indices,generator=torch.Generator().manual_seed(c.seed)) 
-    
-    # leave shuffle off for use of any samplers
-    full_train_loader = DataLoader(transformed_dataset, batch_size=a.args.batch_size,shuffle=False, 
-                        num_workers=0,collate_fn=transformed_dataset.custom_collate_aerial,
-                        pin_memory=False,sampler=full_train_sampler)
-
-    full_val_loader = DataLoader(transformed_dataset, batch_size=a.args.batch_size,shuffle=False, 
-                        num_workers=0,collate_fn=transformed_dataset.custom_collate_aerial,
-                        pin_memory=False,sampler=full_val_sampler)
-    
-    train_loader = DataLoader(transformed_dataset, batch_size=a.args.batch_size,shuffle=False, 
-                        num_workers=0,collate_fn=transformed_dataset.custom_collate_aerial,
-                        pin_memory=False,sampler=train_sampler)
-
-    val_loader = DataLoader(transformed_dataset, batch_size=a.args.batch_size,shuffle=False, 
-                        num_workers=0,collate_fn=transformed_dataset.custom_collate_aerial,
-                        pin_memory=False,sampler=val_sampler)
+    if not a.args.holdout:
+        # create test train split
+        full_train_loader, full_val_loader, train_loader, val_loader = make_loaders(transformed_dataset)
+    else:
+        val_loader = DataLoader(transformed_dataset, batch_size=a.args.batch_size,shuffle=True, 
+                            num_workers=0,collate_fn=transformed_dataset.custom_collate_aerial,
+                            pin_memory=False)
     
     if a.args.mode == 'eval':
         
@@ -98,7 +52,7 @@ if a.args.data == 'cows':
             eval_baselines(mdl,val_loader,mode='val',is_unet_seg=(a.args.model_name=='UNet_seg'))
             #eval_baselines(mdl,train_loader,mode='train',is_unet_seg=(a.args.model_name=='UNet_seg'))
         
-    if a.args.mode == 'train':
+    if a.args.mode == 'train' and not a.args.holdout:
         
         if a.args.bc_only:
             train_classification_head(None,train_loader,val_loader,criterion = nn.CrossEntropyLoss())
