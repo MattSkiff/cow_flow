2aecb1d23f59f478e3676e8d12a3a6c7dfc47f5e
Matthew Skiffington <mks29@students.waikato.ac.nz>
2022-07-08 19:24:27 +1000

    minor dataloader bug fix

--- baselines.py
+++ baselines.py
@@ -311,6 +311,7 @@ class Up(nn.Module):
         # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
         # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
         x = torch.cat([x2, x1], dim=1)
+        
         return self.conv(x)
 
 # https://github.com/leeyeehoo/CSRNet-pytorch- official CSRNet repo
